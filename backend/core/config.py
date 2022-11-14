22a47de0d0ab3339560107c409da96b911f3d439
tejasmahalinga <tejasmahalinga@gmail.com>
2022-11-14 21:48:33 +0530

    connected sqlite db instead of postgres

--- backend/core/config.py
+++ backend/core/config.py
@@ -14,7 +14,11 @@ class Settings:
     POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
     POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
     POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    # DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+    SQLITE_DB_NAME="watches_db.db"
+    DATABASE_URL = f"sqlite:///./{SQLITE_DB_NAME}"
+
 
     SECRET_KEY :str = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
