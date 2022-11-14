36ca769ff416f6432969d7be2f4fa019a9ab5253
tejasmahalinga <tejasmahalinga@gmail.com>
2022-11-14 22:29:45 +0530

    changes in config file, commented .env file dependency

--- backend/core/config.py
+++ backend/core/config.py
@@ -9,11 +9,11 @@ class Settings:
     PROJECT_NAME:str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
 
-    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
-    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
+    # POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    # POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    # POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    # POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
+    # POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
     # DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     SQLITE_DB_NAME="watches_db.db"
